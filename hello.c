8114523cfcd7186d6353aefb3fefa388d6ac90d8
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-02 20:50:28 +0000

    Wed, Aug 2, 2023, 3:50 PM -05:00

--- hello.c
+++ hello.c
@@ -2,6 +2,6 @@
 #include <cs50.h>
 
 int main(void){
-    string answer = get_string("What's your name? ");
-    printf("hello, %s\n", answer);
+    string answer = get_string("What's your name?: ");
+    printf("Hello, %s\n", answer);
 }
