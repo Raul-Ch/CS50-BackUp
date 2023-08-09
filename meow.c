cc7788feda50c5324066926a2d98c9fbf54ae8ef
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-09 03:45:31 +0000

    Tue, Aug 8, 2023, 10:45 PM -05:00

--- meow.c
+++ meow.c
@@ -2,8 +2,19 @@
 
 int main(void)
 {
-    for (int i=1; i>0; i++)
+    for (int i=0; i<3; i++)
     {
         printf("meow\n");
     }
 }
+
+//Infinite Loop
+//Note://You can break from an infinite by hitting control-C on your keyboard.
+
+// int main(void)
+// {
+//     while (true)
+//     {
+//         printf("meow\n");
+//     }
+// }
