11edfe06c2961725e3145acb7933ede5e8068922
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-10 23:56:24 +0000

    Thu, Aug 10, 2023, 6:56 PM -05:00

--- pset1/mario.c
+++ pset1/mario.c
@@ -5,7 +5,7 @@ int main (void)
     const int n = 3;
     for (int i = 0; i < n; i++)
     {
-         for (int i = 0; i < n; i++)
+         for (int j = 0; j < n; j++)
         {
             printf("#");
         }
