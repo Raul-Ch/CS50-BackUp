c4cb2f32dbd75fbac83311d13838efb8caa67038
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-11 00:01:26 +0000

    Thu, Aug 10, 2023, 7:01 PM -05:00

--- pset1/mario.c
+++ pset1/mario.c
@@ -3,8 +3,16 @@
 
 int main (void)
 {
-    //const int n = 3;
-    int n = get_int("Size: ");
+    //Example of a constant: const int n = 3;
+    //Get size of grid
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+
+    //Print grid of Bricks
     for (int i = 0; i < n; i++)
     {
          for (int j = 0; j < n; j++)
