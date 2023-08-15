9f0f9f810f26543d5f97e0f75fefd6f92a6642f8
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-15 02:05:58 +0000

    Mon, Aug 14, 2023, 9:05 PM -05:00

--- half/half.c
+++ half/half.c
@@ -18,19 +18,19 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    //We make a variable by dividing the amount of tax given by 100 to represent %
+    // We make a variable by dividing the amount of tax given by 100 to represent %
     float half_tax = tax / 100;
-    //Then we multiply the % by the amount of the bill
+    // Then we multiply the % by the amount of the bill
     half_tax *= bill;
-    //Finally we get the total of the bill with the tax
+    // Finally we get the total of the bill with the tax
     half_tax += bill;
 
-    //we make a variable of type float, with the int tip cast as a float, to obtain it's %
-    float half_tip = ((float)tip / 100);
-     //Then we multiply the % by the amount of the bill with tax
+    // we make a variable of type float, with the int tip cast as a float, to obtain it's %
+    float half_tip = ((float) tip / 100);
+    // Then we multiply the % by the amount of the bill with tax
     half_tip *= half_tax;
 
-    //At last, we add up the bill with tax and the tip, for out total and we divide it by 2
+    // At last, we add up the bill with tax and the tip, for out total and we divide it by 2
     float total = (half_tip + half_tax) / 2;
 
     return total;
