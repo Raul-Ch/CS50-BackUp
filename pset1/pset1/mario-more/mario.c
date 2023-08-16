8d18f777dee01885150cdf2a98514a379a43aee8
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 04:43:51 +0000

    Tue, Aug 15, 2023, 11:43 PM -05:00

--- pset1/pset1/mario-more/mario.c
+++ pset1/pset1/mario-more/mario.c
@@ -19,7 +19,7 @@ void print_pyramids(int size)
 {
     for(int i = 0; i < size; i++)
     {
-        for(int j = 0; j < size; j++)
+        for(int j = i; j < size; j++)
         {
             printf("#");
         }
