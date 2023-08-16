713eb50b29686d8a99d35be941e03bc32c0ba808
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 19:17:41 +0000

    Wed, Aug 16, 2023, 2:17 PM -05:00

--- pset1/pset1/credit/credit.c
+++ pset1/pset1/credit/credit.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+     // TODO: Prompt for credit card number: "program that prompts the user for a credit card number"
+    int credit_number;
+        credit_number = get_int("Input the credit card number: ");
 }
