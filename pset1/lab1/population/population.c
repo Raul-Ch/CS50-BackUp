ffee1399cb6f1a7d120990f0182e1d787b8995e6
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 02:30:32 +0000

    Tue, Aug 15, 2023, 9:30 PM -05:00

--- pset1/lab1/population/population.c
+++ pset1/lab1/population/population.c
@@ -4,7 +4,11 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
+    do
+    {
+       int star = get_int("Minimum: ");
+    }
+    while (min < 1);
     // TODO: Prompt for end size
 
     // TODO: Calculate number of years until we reach threshold
