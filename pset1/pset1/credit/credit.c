2456deba89142ce453371307d180dcc681018fd5
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 22:14:34 +0000

    Wed, Aug 16, 2023, 5:14 PM -05:00

--- pset1/pset1/credit/credit.c
+++ pset1/pset1/credit/credit.c
@@ -17,7 +17,7 @@ int main(void)
     int credit_length = checksum (credit_number);
     if (credit_length != false)
     {
-        printf()credit_type (credit_number, credit_length);
+        printf("%s",credit_type (credit_number, credit_length));
     }
 }
 
@@ -93,8 +93,6 @@ string credit_type (long credit, int length)
         case 4 :
         return "VISA\n";
         break;
-        default :
-        return "Error\n";
     }
     return 0;
 }
