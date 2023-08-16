628a7ce4d22a7be501f5729d913c7ccaf8e53f59
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 01:42:38 +0000

    Tue, Aug 15, 2023, 8:42 PM -05:00

--- pset1/practice/prime/prime.c
+++ pset1/practice/prime/prime.c
@@ -41,7 +41,7 @@ bool prime(int number)
     // 2.- Any number greater than 2 that is a multiple of 2 is not a prime, since it has at least three divisors:
     // 3.- Any number greater than 3 that is a multiple of 3 is not a prime, since it has 1, 3, and the number itself as divisors.
     // 4.- Any number greater than 5 that is a multiple of 5 is not a prime. (So ​​the only prime number that ends with a 0 or 5 is 5.)
-    else if (number == 1 || number % 2 == 0 || number % 3 == 0)
+    else if (number == 1 || number % 2 == 0 || number % 3 == 0 || number % 5 == 0)
     {
         return false;
     }
