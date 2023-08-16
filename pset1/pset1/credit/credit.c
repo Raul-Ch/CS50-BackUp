794cf4d6cf15cd3853ecd762c728ecadda3e42e7
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 19:22:40 +0000

    Wed, Aug 16, 2023, 2:22 PM -05:00

--- pset1/pset1/credit/credit.c
+++ pset1/pset1/credit/credit.c
@@ -1,13 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+
+
 int main(void)
 {
      // TODO: Prompt for credit card number: "program that prompts the user for a credit card number"
-    double credit_number;
+    long credit_number;
     do
     {
-        credit_number = get_double("Input the credit card number: ");
+        credit_number = get_long("Input the credit card number: ");
     }
     while (credit_number < 1000000000000 || credit_number > 9999999999999999);
+    // "13 to 16 digit numbers"
+
 }
