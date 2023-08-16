f5e41c749b5a74c09b55e03f98df213faaa92e34
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 21:59:19 +0000

    Wed, Aug 16, 2023, 4:59 PM -05:00

--- pset1/pset1/credit/credit.c
+++ pset1/pset1/credit/credit.c
@@ -72,6 +72,13 @@ int checksum (long credit)
 
 int credit_type (long credit, int length)
 {
-    int type = length - 2;
-    long credit /= 
+    length -= 2;
+    int type = 1;
+
+    for (int i = 0; i < length; i++)
+    {
+        type *= 10;
+    }
+    printf("%i\n",type);
+    return 0;
 }
