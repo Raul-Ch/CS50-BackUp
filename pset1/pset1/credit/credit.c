2c0248553a71cd26674e46dc461dd71fabfb7303
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 19:23:46 +0000

    Wed, Aug 16, 2023, 2:23 PM -05:00

--- pset1/pset1/credit/credit.c
+++ pset1/pset1/credit/credit.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
+void checksum (int credit);
 
 int main(void)
 {
@@ -13,5 +13,10 @@ int main(void)
     }
     while (credit_number < 1000000000000 || credit_number > 9999999999999999);
     // "13 to 16 digit numbers"
+    checksum (credit_number);
+}
 
+void checksum (int credit)
+{
+    
 }
