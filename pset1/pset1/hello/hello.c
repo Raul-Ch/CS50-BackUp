9c5b34e68668bb3c1e413d69e0a52c16159be997
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 03:52:04 +0000

    Tue, Aug 15, 2023, 10:52 PM -05:00

--- pset1/pset1/hello/hello.c
+++ pset1/pset1/hello/hello.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
+    // Promt user for his name
     string name = get_string("What's your name?: ");
-    printf("Hello, %s\n", name);
+    // Display greetings
+    printf("Hello, %s!\n", name);
 }
