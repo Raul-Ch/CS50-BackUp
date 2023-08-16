e1d32954217b6c4a9a6d23d8e300ea4afc27a146
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 02:34:51 +0000

    Tue, Aug 15, 2023, 9:34 PM -05:00

--- pset1/lab1/population/population.c
+++ pset1/lab1/population/population.c
@@ -9,9 +9,14 @@ int main(void)
     {
         start = get_int("Input the start size of the population: ");
     }
-    while (start > 9);
+    while (start < 9);
     // TODO: Prompt for end size
-
+    int start;
+    do
+    {
+        start = get_int("Input the start size of the population: ");
+    }
+    while (start < 9);
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
