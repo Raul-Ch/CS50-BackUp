0f2cff72ae61157d9444e7ab6b65e09e211268f5
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 05:45:28 +0000

    Wed, Aug 16, 2023, 12:45 AM -05:00

--- pset1/pset1/mario-more/mario.c
+++ pset1/pset1/mario-more/mario.c
@@ -12,17 +12,24 @@ int main(void)
         size = get_int("Input the size of the pyramids (1-8): ");
     }
     while (size < 0 || size > 8);
+    // "positive integer between, say, 1 and 8, inclusive"
+    // Call the function and pass the size
     print_pyramids(size);
 }
 
+    // Function to print the pyramids
 void print_pyramids(int size)
 {
+    // Cicle to print size (heigth)
     for(int i = 0; i < size; i++)
     {
+        //Cicles to print the width
+        // Here we print out the spaces (Depending on the row is the number of spaces)
         for(int space = i; space < size; space++)
         {
              printf(".");
         }
+        // Here we just print the character (depending on the row) to build the pyramid
         for(int j = 0; j <= i; j++)
         {
              printf("#");
