09451aa946416981834f93094680fd1fc78edfff
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-16 04:01:59 +0000

    Tue, Aug 15, 2023, 11:01 PM -05:00

--- pset1/pset1/mario-more/mario.c
+++ pset1/pset1/mario-more/mario.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // TODO: Prompt for size: " let’s allow the user to decide just how tall the pyramids should be"
+    int size;
+    do
+    {
+        size = get_int("Input the size of the pyramids (1-8): ");
+    }
+    while (size < 0 || size > 8);
 }
