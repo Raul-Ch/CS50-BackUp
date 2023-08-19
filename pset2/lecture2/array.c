fa565759e18e66fa48872c42d03e18a5ef5ba7e5
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-19 18:06:19 +0000

    Sat, Aug 19, 2023, 1:06 PM -05:00

--- pset2/lecture2/array.c
+++ pset2/lecture2/array.c
@@ -4,16 +4,21 @@
 int main(void)
 {
     int length;
+    // Get the length from the user
     do
     {
         length = get_int("Length: ");
     }
     while (length < 1);
-
+    // Declare our array
     int twice[length];
-    
-    for (int i = 0; i < length; i++)
+    // Set the first value
+    twice[0] = 1;
+    printf("%i\n",twice[0]);
+
+    for (int i = 1; i < length; i++)
     {
+        // Make the current element twice the previous and print
         twice[i] = 2 * twice [i - 1];
         printf("%i\n",twice[i]);
     }
