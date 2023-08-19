2248c067db467a9e4c853a15ef63f54c91e5cdb8
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-19 18:17:56 +0000

    Sat, Aug 19, 2023, 1:17 PM -05:00

--- pset2/lecture2/alpha.c
+++ pset2/lecture2/alpha.c
@@ -4,5 +4,16 @@
 
 int main(void)
 {
-    
+    string word = get_string("Word: ");
+    // Length -1 because of the '/0' NUL at the end
+    for(int i = 0; i < strlen(word)-1; i++)
+    {
+        // Checl of NOT alphabetical (i.e., "ba")
+        if (word[i] > word[i + 1]){
+            printf("No\n");
+            return 0;
+        }
+    }
+    printf("Yes\n");
+    return 0;
 }
