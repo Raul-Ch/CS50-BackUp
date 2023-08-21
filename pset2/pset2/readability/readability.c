8acab8a4b510c267d1e572d56396aef8fa5c2a21
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-21 22:46:53 +0000

    Mon, Aug 21, 2023, 5:46 PM -05:00

--- pset2/pset2/readability/readability.c
+++ pset2/pset2/readability/readability.c
@@ -1,7 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int count_letters(string text);
 
 int main(void)
 {
-    string text = printf("Text: ");
+    string text = get_string("Text: ");
+    printf("%i\n",count_letters(text));
+}
+
+int count_letters(string text)
+{
+    int letters = 0;
+    for (int i = 0; i <= strlen(text); i++)
+    {
+        if (text[i])
+        {
+            letters++;
+        }
+    }
+    return letters;
 }
