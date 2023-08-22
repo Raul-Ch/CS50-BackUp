d9342f43fea6117d27431cfad1c1d2ac50dbab05
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-22 03:33:12 +0000

    Mon, Aug 21, 2023, 10:33 PM -05:00

--- pset2/pset2/wordle/wordle.c
+++ pset2/pset2/wordle/wordle.c
@@ -38,7 +38,7 @@ int main(int argc, string argv[])
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
-    if (argv[1] < 53 || argv[1] > 56)
+    if (argv[1] < 53)
     {
         printf("Usage: ./wordle wordsize\n");
     }
