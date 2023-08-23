d9fd04d3ffd9f2d7d0879fe28bff708a53c40e35
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-23 02:57:20 +0000

    Tue, Aug 22, 2023, 9:57 PM -05:00

--- pset2/pset2/wordle/wordle.c
+++ pset2/pset2/wordle/wordle.c
@@ -121,6 +121,11 @@ string get_guess(int wordsize)
 
     // ensure users actually provide a guess that is the correct length
     // TODO #3
+    do
+    {
+
+    }
+    while (!strlen(wordsize))
 
     return guess;
 }
