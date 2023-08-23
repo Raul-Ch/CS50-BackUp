6dda8b9acf30992d30acb5e4c29c5851d06fcfc1
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-23 04:47:10 +0000

    Tue, Aug 22, 2023, 11:47 PM -05:00

--- pset2/pset2/wordle/wordle.c
+++ pset2/pset2/wordle/wordle.c
@@ -156,14 +156,13 @@ int check_word(string guess, int wordsize, int status[], string choice)
 
     for (int i = 0; i <= wordsize; i++)
     {
+        
         for (int j = 0; j <= wordsize; j++)
         {
             if (guess[i] == choice[i])
             {
                 status [i] = EXACT;
                 score += EXACT;
-                i++;
-                j++;
                 break;
             }
             if (guess[i] == choice[j])
