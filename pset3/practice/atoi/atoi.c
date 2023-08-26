bb432db8419b1f664b9a85e6e64c064d791f60fd
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-26 03:12:59 +0000

    Fri, Aug 25, 2023, 10:12 PM -05:00

--- pset3/practice/atoi/atoi.c
+++ pset3/practice/atoi/atoi.c
@@ -27,7 +27,11 @@ int convert(string input)
 {
     // TODO
     // a - '0' is equivalent to ((int)a) - ((int)'0'), which means the ascii values of the characters are subtracted from each other.
-    // The idea is to use the ASCII value of the digits from 0 to 9 start from 48 – 57. 
-    int last = input[0] - 48;
+    // The idea is to use the ASCII value of the digits from 0 to 9 start from 48 – 57.
+    if (n < 0)
+    {
+        return;
+    }
+    int last = input[] - 48;
     return last;
 }
