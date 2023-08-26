561ec32a7cd16e8e8593def41a509e3b8a99bc27
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-26 03:36:13 +0000

    Fri, Aug 25, 2023, 10:36 PM -05:00

--- pset3/practice/atoi/atoi.c
+++ pset3/practice/atoi/atoi.c
@@ -32,7 +32,6 @@ int convert(string input)
     }
     // a - '0' is equivalent to ((int)a) - ((int)'0'), which means the ascii values of the characters are subtracted from each other.
     // The idea is to use the ASCII value of the digits from 0 to 9 start from 48 – 57.
-    int new = input[ strlen(input) - 1] - 48;
-    new +=  pow10(strlen(input) - 1)*new;
-    convert (input);
+    int index = strlen(input) - 1;
+    int numeric = input[index] - 48;
 }
