07aba6a8aa04384143b8b2badbc7dfd58b31b7b0
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-28 01:28:27 +0000

    Sun, Aug 27, 2023, 8:28 PM -05:00

--- pset3/practice/max/max.c
+++ pset3/practice/max/max.c
@@ -11,7 +11,7 @@ int main(void)
     do
     {
         n = get_int("Number of elements: ");
-    } 
+    }
     while (n < 1);
 
     int arr[n];
@@ -27,5 +27,15 @@ int main(void)
 // TODO: return the max value
 int max(int array[], int n)
 {
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if (array[j] > array[i])
+            {
+                int max = array[j];
+            }
+        }
+    }
     return 0;
 }
