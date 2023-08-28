809d086d0e14ffbb24ea69ad954ae1831494fa06
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-28 20:52:07 +0000

    Mon, Aug 28, 2023, 3:52 PM -05:00

--- pset3/pset3/plurality/plurality.c
+++ pset3/pset3/plurality/plurality.c
@@ -66,14 +66,13 @@ int main(int argc, string argv[])
 bool vote(string name)
 {
     // TODO
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(name, candidates[i].name) == 0)
         {
             candidates[i].votes += 1;
             return true;
         }
-        else if
     }
     return false;
 }
