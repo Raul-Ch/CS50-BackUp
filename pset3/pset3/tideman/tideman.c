7f25e1b4fd2e98285dfd9caf0f221d92de34855d
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-28 23:48:58 +0000

    Mon, Aug 28, 2023, 6:48 PM -05:00

--- pset3/pset3/tideman/tideman.c
+++ pset3/pset3/tideman/tideman.c
@@ -121,7 +121,7 @@ void record_preferences(int ranks[])
         for (int j = 0; j < candidate_count; j++)
         {
             printf("This is ranks %i: %i\n",i, ranks[j]);
-            preferences[i][j] = preferences[i][ranks[j]];
+            preferences[i][j] = ranks[j];
             i++;
         }
     }
