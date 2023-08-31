35ae9a2dd238d681a2aeebd26a4c113148dd4974
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-31 00:06:39 +0000

    Wed, Aug 30, 2023, 7:06 PM -05:00

--- pset4/lecture4/addresses.c
+++ pset4/lecture4/addresses.c
@@ -3,9 +3,18 @@
 int main(void)
 {
     int n = 50;
-    int *p = &n;
 
+    // Print value in n
     printf("%i\n", n);
+    // Print:
+    // & Provides the address of something stored in memory.
+    // * Instructs the compiler to go to a location in memory.
+    // Notice the %p, which allows us to view the address of a location in memory.
     printf("%p\n",&n);
+
+    int *p = &n;
+    //Notice that p is a pointer that contains a number that is the address of an integer n.
     printf("%p\n",p);
+    // To illustrate the use of the * operator (prints the integer at the location of p.)
+    printf("%i\n", *p);
 }
