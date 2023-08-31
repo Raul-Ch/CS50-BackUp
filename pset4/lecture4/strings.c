e7c4e4658b5e5a45494c1f5b0287ec6abf165e04
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-31 00:10:15 +0000

    Wed, Aug 30, 2023, 7:10 PM -05:00

--- pset4/lecture4/strings.c
+++ pset4/lecture4/strings.c
@@ -3,10 +3,15 @@
 
 int main(void)
 {
+    // prints the memory locations of each character in the string s.
     string s = "HI!";
     printf("%p\n", s);
     printf("%p\n", &s[0]);
     printf("%p\n", &s[1]);
     printf("%p\n", &s[2]);
     printf("%p\n", &s[3]);
+
+    // Present the string that starts at the location of s.
+    char *st = "HI!";
+    printf("%c\n", st);
 }
