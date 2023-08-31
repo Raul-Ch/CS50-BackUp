9dbaaf66581c55654ef0579646bdff8c2ed48a7f
Raul Chimal <raulchimalhdz@hotmail.com>
2023-08-31 00:31:44 +0000

    Wed, Aug 30, 2023, 7:31 PM -05:00

--- pset4/lecture4/copy.c
+++ pset4/lecture4/copy.c
@@ -20,5 +20,29 @@ int main (void)
 
     // Print string twice
     printf("s: %p\n", s);
+    printf("s: %s\n", s);
     printf("t: %p\n", t);
+    printf("t: %s\n\n", t);
+
+    // Crete an authentic copy of our string as follows:
+    char *copy = get_string("\nReal Copys: ");
+
+    // Allocate memory for another string
+    char *t = malloc(strlen(copy) + 1);
+    // Notice that malloc(strlen(s) + 1) creates a block of memory that is the length of the string s plus one.
+    // This allows for the inclusion of the null \0 character in our final, copied string.
+
+    // Copy string into memory, including '\0'
+    for (int i = 0; i <= strlen(s); i++)
+    {
+        t[i] = copy[i];
+    }
+
+    // Capitalize copy
+    t[0] = toupper(t[0]);
+
+    // Print strings
+    printf("s: %s\n", copy);
+    printf("t: %s\n", t);
+
 }
