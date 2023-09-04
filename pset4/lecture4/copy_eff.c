049ba5f4fb19693949306f435341e7330d12d91c
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-04 21:36:34 +0000

    Mon, Sep 4, 2023, 4:36 PM -05:00

--- pset4/lecture4/copy_eff.c
+++ pset4/lecture4/copy_eff.c
@@ -7,22 +7,36 @@
 int main (void)
 {
     // Get a string
+
     char *s = get_string("s: ");
+    if (s == NULL)
+    {
+        return 1;
+    }
 
     // Allocate memory for another string
     char *t = malloc(strlen(s) + 1);
+    if (t == NULL)
+    {
+        return 1;
+    }
 
     // Copy string into memory
     strcpy(t, s);
     // Notice that strcpy does the same work that our for loop previously did.
-    
-    // Capitalize copy
-    t[0] = toupper(t[0]);
+    if (strlen(t) > 0)
+    {
+        // Capitalize copy
+        t[0] = toupper(t[0]);
+    }
 
     // Print strings
     printf("s: %s\n", s);
     printf("s: %p\n", s);
     printf("t: %s\n", t);
     printf("s: %p\n", t);
-
+    
+    // Free memory
+    free(t);
+    return 0;
 }
