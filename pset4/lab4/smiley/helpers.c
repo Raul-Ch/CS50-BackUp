56d81708447a78eb6e9c1d3d43c6890dbf6ce18e
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-12 18:31:25 +0000

    Tue, Sep 12, 2023, 12:31 PM -06:00

--- pset4/lab4/smiley/helpers.c
+++ pset4/lab4/smiley/helpers.c
@@ -3,13 +3,16 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    // https://www.bluemarblegeo.com/knowledgebase/global-mapper-24/images/Examples/palette/spectrum.png
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            if(image[i][j].rgbtBlue == (0) && image[i][j].rgbtGreen == (0) && image[i][j].rgbtRed == (0))
+            if(image[i][j].rgbtBlue == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtRed == 0)
             {
-                image[i][j].rgbtBlue = (20);
+                image[i][j].rgbtBlue = 0;
+                image[i][j].rgbtGreen = 105;
+                image[i][j].rgbtRed = 0;
             }
         }
     }
