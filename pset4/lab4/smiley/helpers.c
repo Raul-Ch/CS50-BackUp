a9870fb8f62578043667ad5230c5227528aba3f5
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-12 18:29:24 +0000

    Tue, Sep 12, 2023, 12:29 PM -06:00

--- pset4/lab4/smiley/helpers.c
+++ pset4/lab4/smiley/helpers.c
@@ -7,7 +7,7 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if(image[i][j].rgbtBlue == (255) && image[i][j].rgbtGreen == (255) && image[i][j].rgbtRed == (255))
+            if(image[i][j].rgbtBlue == (0) && image[i][j].rgbtGreen == (0) && image[i][j].rgbtRed == (0))
             {
                 image[i][j].rgbtBlue = (20);
             }
