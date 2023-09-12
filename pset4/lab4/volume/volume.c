150c93cbe95c9c9a6920fcbd41f650d774f1ad70
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-12 20:00:24 +0000

    Tue, Sep 12, 2023, 2:00 PM -06:00

--- pset4/lab4/volume/volume.c
+++ pset4/lab4/volume/volume.c
@@ -48,6 +48,8 @@ int main(int argc, char *argv[])
         printf("%i\n",header[i]);
     }
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    fread(buffer[HEADER_SIZE],sizeof(*buffer),HEADER_SIZE,input);
 
     // Close files
     fclose(input);
