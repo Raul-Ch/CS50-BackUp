1323b65e4e10fcd7eda250916ad34c42945d6048
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-12 19:38:04 +0000

    Tue, Sep 12, 2023, 1:38 PM -06:00

--- pset4/lab4/volume/volume.c
+++ pset4/lab4/volume/volume.c
@@ -36,9 +36,10 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
 
+    fread(header,HEADER_SIZE,1,input);
+
     for (int i = 0; i < HEADER_SIZE; i++)
     {
-        fread(header,HEADER_SIZE,1,input);
         printf("%i",header[i]);
     }
     // TODO: Read samples from input file and write updated data to output file
