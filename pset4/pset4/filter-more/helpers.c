5614e9e8e93f1b78b06e3e1eb904491098bb82a1
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-14 00:23:01 +0000

    Wed, Sep 13, 2023, 6:23 PM -06:00

--- pset4/pset4/filter-more/helpers.c
+++ pset4/pset4/filter-more/helpers.c
@@ -1,15 +1,16 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    int average = 0;
+    float average = 0;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            average = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
-            average /= 3;
+            average = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed / 3;
+            average = round(average);
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtRed = average;
