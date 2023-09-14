606952dda770fc130320cfaa59bf323cb1008681
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-14 00:41:13 +0000

    Wed, Sep 13, 2023, 6:41 PM -06:00

--- pset4/pset4/filter-more/helpers.c
+++ pset4/pset4/filter-more/helpers.c
@@ -22,11 +22,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE temp;
-    float mirror = round(width / 2);
     for (int i = 0; i < height; i++)
     {
         // we divide the width by two because as we change de pixels at the middle there are all changed
-        for (int j = 0; j < mirror; j++)
+        for (int j = 0; j < width - j; j++)
         {
             // Saving last pixel
             temp = image[i][width - j];
