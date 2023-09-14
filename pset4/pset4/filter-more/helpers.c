e53e5524b1d135940811bc7a099de2b3d9ac7833
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-14 00:24:29 +0000

    Wed, Sep 13, 2023, 6:24 PM -06:00

--- pset4/pset4/filter-more/helpers.c
+++ pset4/pset4/filter-more/helpers.c
@@ -9,7 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            average = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed / 3;
+            average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
             average = round(average);
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
