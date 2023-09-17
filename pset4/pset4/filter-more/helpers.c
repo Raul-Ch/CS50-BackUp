b6dd7c115c7ff5f31b6282fefca612bc26b1888b
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-17 00:15:39 +0000

    Sat, Sep 16, 2023, 6:15 PM -06:00

--- pset4/pset4/filter-more/helpers.c
+++ pset4/pset4/filter-more/helpers.c
@@ -62,7 +62,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if((i == 0 && j == 0) || (i == 0 && j == width - 1) || (i == height - 1 && j == 0) || (i == height - 1 && j == width - 1))
+            //if((i == 0 && j == 0) || (i == 0 && j == width - 1) || (i == height - 1 && j == 0) || (i == height - 1 && j == width - 1))
+            else if(j - 1 < 0 || j + 1 >= width)
             {
 
             }
