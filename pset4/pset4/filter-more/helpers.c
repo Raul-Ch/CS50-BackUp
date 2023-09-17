4dd78ff1f24d54e979c7689a0c9643c3958bb7e9
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-17 00:42:13 +0000

    Sat, Sep 16, 2023, 6:42 PM -06:00

--- pset4/pset4/filter-more/helpers.c
+++ pset4/pset4/filter-more/helpers.c
@@ -89,14 +89,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             }
         }
     }
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            free(temp_image[i][j]);
-        }
-    }
+        free(temp_image);
     return;
 }
 
