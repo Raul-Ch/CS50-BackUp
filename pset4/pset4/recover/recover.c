a5beb64ac07fef8d5e8998f36e7dd959e61c65fa
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-18 21:38:01 +0000

    Mon, Sep 18, 2023, 3:38 PM -06:00

--- pset4/pset4/recover/recover.c
+++ pset4/pset4/recover/recover.c
@@ -3,10 +3,18 @@
 
 int main(int argc, char *argv[])
 {
+    // Correct Usage
     if (argc != 2)
     {
         printf("Usage: ./recover IMAGE\n");
         return 1;
     }
 
+    // Open input file
+    FILE *f = fopen(argv[1],"r");
+    if (f == NULL)
+    {
+        printf("Could not open file: %s.\n", f);
+        return 4;
+    }
 }
