53689e84b391657d32dacf9c28c110093870fe87
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-18 23:53:05 +0000

    Mon, Sep 18, 2023, 5:53 PM -06:00

--- pset4/pset4/recover/recover.c
+++ pset4/pset4/recover/recover.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
         // ..else ....
         if (buffer[0] == 0xff && buffer[1] == 0xd8  && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            char *filename = "";
+            char *filename = malloc[3];
             // Implement jpeg format of name starting at 000.jpeg
             sprintf(filename, "%03i.jpg",number_jpg);
 
