d3502ee9098082acae1ee149517b629b73275eea
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-18 23:56:47 +0000

    Mon, Sep 18, 2023, 5:56 PM -06:00

--- pset4/pset4/recover/recover.c
+++ pset4/pset4/recover/recover.c
@@ -36,7 +36,8 @@ int main(int argc, char *argv[])
         // ..else ....
         if (buffer[0] == 0xff && buffer[1] == 0xd8  && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            char *filename = malloc(3 * sizeof(int));
+            // You need space for three digits, the extension, and the null terminator
+            char *filename = malloc(8 * sizeof(char));
             // Implement jpeg format of name starting at 000.jpeg
             sprintf(filename, "%03i.jpg",number_jpg);
 
@@ -49,8 +50,8 @@ int main(int argc, char *argv[])
             // If already found JPEG
             // Close any remaining file
             fclose(jpeg);
+            free(filename);
         }
     }
     fclose(f);
-
 }
