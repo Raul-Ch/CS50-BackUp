046a635764fb7b5aa059cd71e062190489e23c86
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-19 00:21:28 +0000

    Mon, Sep 18, 2023, 6:21 PM -06:00

--- pset4/pset4/recover/recover.c
+++ pset4/pset4/recover/recover.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
         }
         else
         {
-            if()
+            if(buffer[0] == 0xff && buffer[1] == 0xd8  && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
             {
                 // If already found JPEG
                 // Close any remaining file
