2dd4e0a174704307f7bcff66a59c8ef49bd78a3b
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-19 18:04:41 +0000

    Tue, Sep 19, 2023, 12:04 PM -06:00

--- pset4/pset4/recover/recover.c
+++ pset4/pset4/recover/recover.c
@@ -1,6 +1,6 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 // Number of bytes in .JPEG header
 const int BYTES_SIZE = 512;
@@ -15,11 +15,10 @@ int main(int argc, char *argv[])
     }
 
     // Open memory card
-    FILE *f = fopen(argv[1],"r");
+    FILE *f = fopen(argv[1], "r");
 
     // Prepare files
     FILE *jpeg = NULL;
-
 
     if (f == NULL)
     {
@@ -34,41 +33,41 @@ int main(int argc, char *argv[])
 
     int number_jpg = 0;
 
+    // You need space for three digits, the extension, and the null terminator
+    char *filename = malloc(8 * sizeof(char));
+
     while (fread(&buffer, 1, BYTES_SIZE, f) == BYTES_SIZE)
     {
         // If first JPEG
         // ..else ....
-        if (buffer[0] == 0xff && buffer[1] == 0xd8  && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // You need space for three digits, the extension, and the null terminator
-            char *filename = malloc(8 * sizeof(char));
-             // Implement jpeg format of name starting at 000.jpeg
-            sprintf(filename, "%03i.jpg",number_jpg);
+            // Implement jpeg format of name starting at 000.jpeg
+            sprintf(filename, "%03i.jpg", number_jpg);
 
             // Create that file
-            jpeg = fopen(filename,"w");
-            free(filename);
-            
-            if(number_jpg == 0)
+            jpeg = fopen(filename, "w");
+
+            if (number_jpg == 0)
             {
-                number_jpg ++;
+                number_jpg++;
                 // Write on the file
                 fwrite(&buffer, 1, BYTES_SIZE, jpeg);
             }
 
             else
             {
-            fclose(jpeg);
-            number_jpg ++;
+                fclose(jpeg);
+                number_jpg++;
 
-            // Implement jpeg format of name starting at 000.jpeg
-            sprintf(filename, "%03i.jpg",number_jpg);
+                // Implement jpeg format of name starting at 000.jpeg
+                sprintf(filename, "%03i.jpg", number_jpg);
 
-            // Create that file
-            jpeg = fopen(filename,"w");
+                // Create that file
+                jpeg = fopen(filename, "w");
 
-            // Write on the file
-            fwrite(&buffer, 1, BYTES_SIZE, jpeg);
+                // Write on the file
+                fwrite(&buffer, 1, BYTES_SIZE, jpeg);
             }
         }
         else
@@ -80,5 +79,6 @@ int main(int argc, char *argv[])
         }
     }
     fclose(jpeg);
+    free(filename);
     fclose(f);
 }
