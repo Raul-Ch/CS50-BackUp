21c33b63791ebd98d0a9af8f9907f7a0a31e4ff1
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-21 00:35:54 +0000

    Wed, Sep 20, 2023, 6:35 PM -06:00

--- pset5/node.c
+++ pset5/node.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definition of a new data type called node using typedef.
 typedef struct node
 {
     int number;
@@ -11,4 +12,13 @@ typedef struct node
 }
 node;
 
-int main()
+// Struct node is a structure that has two elements: an integer number and a pointer next to another struct node.
+// Each node contains some data (number in this case)
+// and a pointer to the next node in the list (next).
+// If next is NULL, it means this node is the last one in the list.
+
+
+int main(int argc, char *argv[])
+{
+
+}
