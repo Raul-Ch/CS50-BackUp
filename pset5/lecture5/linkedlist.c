6cddc395557bb4db0912c377b18d7fe1a3107ff4
Raul Chimal <raulchimalhdz@hotmail.com>
2023-09-23 22:48:54 +0000

    Sat, Sep 23, 2023, 4:48 PM -06:00

---
+++ pset5/lecture5/linkedlist.c
@@ -0,0 +1,37 @@
+// Implements a list of numbers using a linked list
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef stuct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(int argc, char *argv[])
+{
+    // Memory for numbers
+    node *list = NULL;
+
+    // For each command-line argument
+    for (int i = 1; i < argc; i++)
+    {
+        // Convert argument to int
+        int number = atoi(argv[i]);
+
+        // Allocate node for number
+        node *n = malloc(sizeof(node));
+        if (n = NULL)
+        {
+            return 1;
+        }
+        // *n.number = number->;
+        n->number = number;
+
+
+    }
+
+}
