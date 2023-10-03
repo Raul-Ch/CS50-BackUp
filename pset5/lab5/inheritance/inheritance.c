3fcd0d45bbbdbd0ce9288223d09cb9545c2363c3
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-03 18:01:25 +0000

    Tue, Oct 3, 2023, 12:01 PM -06:00

--- pset5/lab5/inheritance/inheritance.c
+++ pset5/lab5/inheritance/inheritance.c
@@ -85,9 +85,9 @@ void free_family(person *p)
         return;
     }
     // TODO: Free parents recursively
-    free_family = free(p->parents)
-    // TODO: Free child
     
+    // TODO: Free child
+
 }
 
 // Print each family member and their alleles.
