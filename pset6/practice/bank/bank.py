5f9549339dd176ec8a7bb1b9a4b7945d723611aa
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-09 23:59:19 +0000

    Mon, Oct 9, 2023, 5:59 PM -06:00

--- pset6/practice/bank/bank.py
+++ pset6/practice/bank/bank.py
@@ -5,8 +5,8 @@ from sys import argv
 for check in argv:
     if argv[1] == 'Hello':
         print("$0")
-    elif:
-        for checkh in argv[2]
+    elif argv[1] not 'Hello':
+        for checkh in argv[1]
             if checkh == 'H':
                     print("$20")
     else:
