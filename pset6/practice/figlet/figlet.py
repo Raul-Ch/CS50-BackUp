8b003fa6343e8a50c4e17b8b6901135f4aae8de8
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-10 16:51:58 +0000

    Tue, Oct 10, 2023, 10:51 AM -06:00

--- pset6/practice/figlet/figlet.py
+++ pset6/practice/figlet/figlet.py
@@ -2,23 +2,22 @@ from sys import argv, exit
 # The documentation for pyfiglet isn’t very clear, but you can use the module as follows:
 from pyfiglet import Figlet
 
+figlet = Figlet()
+
 if len(argv) > 3:
     exit("Invalid usage")
 
 if argv[1] not in ["-f","--font"]:
      exit("Invalid usage")
 
+# You can then get a list of available fonts with code like this:
+if argv[2] not in [figlet.getFonts()]:
+      exit("Invalid usage")
+else:
+    # You can set the font with code like this, wherein f is the font’s name as a str:
+    figlet.setFont(font=argv[2])
+
 input("Input: ")
 
-figlet = Figlet()
-
-
-# You can then get a list of available fonts with code like this:
-figlet.getFonts()
-
-# You can set the font with code like this, wherein f is the font’s name as a str:
-figlet.setFont(font=f)
-
-
 # And you can output text in that font with code like this, wherein s is that text as a str:
-print(figlet.renderText(s))
+print("Output", figlet.renderText(s))
