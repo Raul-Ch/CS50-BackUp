bbb558e8ae445fa44956c3bfcfec6e109616509b
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-10 19:52:18 +0000

    Tue, Oct 10, 2023, 1:52 PM -06:00

--- pset6/practice/jar/jar.py
+++ pset6/practice/jar/jar.py
@@ -9,27 +9,29 @@ class Jar:
     # This method returns a string representation of the object.
     # It's what gets printed when you print an instance of the class.
     def __str__(self):
+        """Finally, the whole expression is wrapped in {} inside the f-string, which means
+        "evaluate this expression and convert the result to a string" """
         return f"{self.size * '🍪'}"
 
     # This method could be used to add a certain number (n) of items to the jar.
     def deposit(self, n):
-        ...
+       self.size += n
 
 
     # This method could be used to remove a certain number (n) of items from the jar.
     def withdraw(self, n):
-        ...
+        self.size -= n
 
 
     # This is a decorator that allows a method to be accessed like an attribute instead of a method.
     @property
     # This method, decorated with @property, could be used to get the capacity of the jar.
     def capacity(self):
-        ...
+        return self.capactity
 
     @property
     # This method, also decorated with @property, could be used to get the current number of items in the jar.
     def size(self):
-        ...
+        return self.size
 
 def main():
