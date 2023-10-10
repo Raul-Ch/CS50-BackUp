e4a137c4b71e2e927a62215f31dcdbd48453119f
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-10 19:01:43 +0000

    Tue, Oct 10, 2023, 1:01 PM -06:00

--- pset6/practice/figlet/figlet.py
+++ pset6/practice/figlet/figlet.py
@@ -5,15 +5,15 @@ from pyfiglet import Figlet
 figlet = Figlet()
 fonts = figlet.getFonts():
 
-if len(argv) >= 1 or len(argv) <= 2:
-    if argv[1] not in ["-f","--font"]:
+if len(argv) > 1 or len(argv) < 4:
+    if argv[2] not in ["-f","--font"]:
         exit("Invalid usage")
     else:
-         figlet.setFont(fonts[random.randrange()])
+         figlet.setFont(fonts[random.randrange(len(fonts))])
 
-    if len(argv) > 1:
-        if argv[2] in fonts:
-            figlet.setFont(font = argv[2])
+    if len(argv) > 2:
+        if argv[3] in fonts:
+            figlet.setFont(font = argv[3])
         else:
             exit("Invalid usage")
 
