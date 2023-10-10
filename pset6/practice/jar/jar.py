6947920ec54d7bcd3667ba2b8b27bb1ba1f59d3a
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-10 19:57:50 +0000

    Tue, Oct 10, 2023, 1:57 PM -06:00

--- pset6/practice/jar/jar.py
+++ pset6/practice/jar/jar.py
@@ -5,6 +5,8 @@ class Jar:
     def __init__(self, capacity=12):
         self.capacity = capacity
         self.size = 0
+        if capacity < 0:
+            raise ValueError
 
     # This method returns a string representation of the object.
     # It's what gets printed when you print an instance of the class.
@@ -15,23 +17,27 @@ class Jar:
 
     # This method could be used to add a certain number (n) of items to the jar.
     def deposit(self, n):
-       self.size += n
+       if self.size += n > self.capacity:
+            raise ValueError
+       else self.size += n
 
 
     # This method could be used to remove a certain number (n) of items from the jar.
     def withdraw(self, n):
-        self.size -= n
+       if self.size -= n < self.capacity0::
+            raise ValueError
+       else self.size -= n
 
 
     # This is a decorator that allows a method to be accessed like an attribute instead of a method.
     @property
     # This method, decorated with @property, could be used to get the capacity of the jar.
     def capacity(self):
-        return self.capactity
+        return self.capacity
 
     @property
     # This method, also decorated with @property, could be used to get the current number of items in the jar.
     def size(self):
-        return self.size
+        return size
 
 def main():
