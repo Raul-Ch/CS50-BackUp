95d1c17cee375a51a0b9c79f7a0f9c470867790b
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-10 15:54:52 +0000

    Tue, Oct 10, 2023, 9:54 AM -06:00

--- pset6/practice/bank/bank.py
+++ pset6/practice/bank/bank.py
@@ -1,9 +1,12 @@
-# Prints a command-line argument
+# In a file called bank.py, implement a program that prompts the user for a greeting.
+# If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”),
+# output $20. Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat
+# the user’s greeting case-insensitively.
 
 from sys import argv
 
 for check in argv:
-    if argv[1] == 'Hello':
+    if check == 'Hello':
         print("$0")
         for checkh in argv[1]:
             if checkh == 'H':
