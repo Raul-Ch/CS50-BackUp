2b2cbb6a9a567598201cd8affba0924d6d4ceb52
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-10 23:26:12 +0000

    Tue, Oct 10, 2023, 5:26 PM -06:00

--- pset6/practice/seven-day-average/seven-day-average.py
+++ pset6/practice/seven-day-average/seven-day-average.py
@@ -43,7 +43,7 @@ def calculate(reader):
 
         if state in new_cases:
             # append new cases to new_cases[state]
-            cases.append(new_cases)
+            state.append(cases)
 
         else:
             # initialize new_cases[state] as a list and add new cases
