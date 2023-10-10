13e77e390d4b6bfa01f9d95539d623c8198683f1
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-10 16:56:36 +0000

    Tue, Oct 10, 2023, 10:56 AM -06:00

--- pset6/practice/figlet/figlet.py
+++ pset6/practice/figlet/figlet.py
@@ -4,20 +4,22 @@ from pyfiglet import Figlet
 
 figlet = Figlet()
 
-if len(argv) > 3:
-    exit("Invalid usage")
+if len(argv) == 1:
+    if argv[1] not in ["-f","--font"]:
+        break
 
-if argv[1] not in ["-f","--font"]:
-     exit("Invalid usage")
-
-# You can then get a list of available fonts with code like this:
-if argv[2] not in [figlet.getFonts()]:
-      exit("Invalid usage")
-else:
-    # You can set the font with code like this, wherein f is the font’s name as a str:
-    figlet.setFont(font=argv[2])
+elif leng(argv) == 2:
+    # You can then get a list of available fonts with code like this:
+    if argv[2] not in [figlet.getFonts()]:
+        break
+    else:
+        # You can set the font with code like this, wherein f is the font’s name as a str:
+        figlet.setFont(font=argv[2])
 
 input("Input: ")
 
 # And you can output text in that font with code like this, wherein s is that text as a str:
 print("Output", figlet.renderText(s))
+
+else
+    exit("Invalid usage")
