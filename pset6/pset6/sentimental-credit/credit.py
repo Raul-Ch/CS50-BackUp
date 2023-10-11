d2d865cd8124e7fd24b379525e3eb95f4dd40dba
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-11 23:57:26 +0000

    Wed, Oct 11, 2023, 5:57 PM -06:00

--- pset6/pset6/sentimental-credit/credit.py
+++ pset6/pset6/sentimental-credit/credit.py
@@ -1,7 +1,12 @@
 # TODO
 from cs50 import get_int
 
-credit_number = get_long("Input the credit card number: ")
+def checksum(credit):
+    while(credit > 0):
+        print("ok")
 
+
+credit_number = get_long("Input the credit card number: ")
+checksum(credit_number)
 
 
