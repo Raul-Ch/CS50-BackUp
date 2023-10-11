8a4faed9eadd0d043c712fe199fc556a5fb6d159
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-11 19:42:59 +0000

    Wed, Oct 11, 2023, 1:42 PM -06:00

--- pset6/lab6/world-cup/tournament.py
+++ pset6/lab6/world-cup/tournament.py
@@ -21,7 +21,7 @@ def main():
       reader = csv.DictReader(file)
 
       for row in reader:
-          teams.append = row['team']
+          teams.append(row['team'])
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
