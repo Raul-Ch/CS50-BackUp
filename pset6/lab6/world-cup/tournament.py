99dfd913364e242bb0231b8dd49f827974ac2298
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-11 20:26:11 +0000

    Wed, Oct 11, 2023, 2:26 PM -06:00

--- pset6/lab6/world-cup/tournament.py
+++ pset6/lab6/world-cup/tournament.py
@@ -37,8 +37,7 @@ def main():
     # In this case, you don't need to use the loop variable itself; you just want to repeat the loop N times.
     for _ in range(N):
         team_name = simulate_tournament(teams)
-        if team_name in counts:
-            counts[team_name] += 1
+        counts[team_name] = counts.get(team_name, 0) + 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -70,12 +69,13 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    while (len(simulate_round(teams) > 1):
+    while (len(teams) > 1):
         # Update teams with the winners of each round.
         teams = simulate_round(teams)
 
     # return the name of the winning team, not the list teams.
     # You can access the name of the winning team with teams[0]["name"]
+    print(teams[0]["name"])
     return teams[0]["name"]
 
 
