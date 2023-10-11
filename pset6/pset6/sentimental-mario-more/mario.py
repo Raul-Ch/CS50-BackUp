0f4674f111b473705f9d8b3ab240ce8007fd6d78
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-11 22:07:58 +0000

    Wed, Oct 11, 2023, 4:07 PM -06:00

--- pset6/pset6/sentimental-mario-more/mario.py
+++ pset6/pset6/sentimental-mario-more/mario.py
@@ -1,6 +1,12 @@
 # TODO
 from cs50 import get_int
 
-height = int(input("Height: "))
-while not 1 <= height <= 8:
-        height = int(input("Height: "))
+# prompt the user with get_int
+# positive integer between 1 and 8, inclusive
+# re-prompt if wrong
+while True:
+    height = int(input("Height: "))
+    if 1 <= height <= 8:
+        break
+
+ for (int i = 0; i < size; i++)
