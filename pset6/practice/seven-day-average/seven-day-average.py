20e8506d707e9e901878fe80c3a50fb853ce21fa
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-11 18:01:01 +0000

    Wed, Oct 11, 2023, 12:01 PM -06:00

--- pset6/practice/seven-day-average/seven-day-average.py
+++ pset6/practice/seven-day-average/seven-day-average.py
@@ -71,8 +71,9 @@ def calculate(reader):
 # TODO: Calculate and print out seven day average for given state
 def comparative_averages(new_cases, states):
     for state in new_cases:
-        print("State: ", state)
-        print("New cases: ", new_cases[state])
+        sum += new_cases[state]
+
+    print(sum)
 
 
 main()
