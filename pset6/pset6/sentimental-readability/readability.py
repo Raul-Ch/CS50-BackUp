24f49c3c7f600e5d3bd04506f507d2034ce848e6
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-12 21:47:37 +0000

    Thu, Oct 12, 2023, 3:47 PM -06:00

--- pset6/pset6/sentimental-readability/readability.py
+++ pset6/pset6/sentimental-readability/readability.py
@@ -14,9 +14,25 @@ words = len(word)
 letters = 0
 sentences = 0
 
-for word in words:
-    letters += len(word)
-    if word.endswith(('.', '!', '?')):
+for letter in word:
+    letters += len(letter)
+    if letter.endswith(('.', '!', '?')):
         sentences += 1
 
-print(words, letters, sentences)
+"""print(words, letters, sentences)"""
+
+# "Where 'L' is the average number of letters per 100 words in the text"
+L = (letters / words) * 100
+# And 'S' is the average number of sentences per 100 words in the text."
+S = (sentences / words) * 100
+
+CL_Index = 0.0588 * L - 0.296 * S - 15.8
+
+if (CL_Index < 1):
+    print("Before Grade 1\n")
+
+elif (CL_Index > 16):
+    print("Grade 16+\n")
+
+else:
+    print(f"Grade: {int(CL_Index)}")
