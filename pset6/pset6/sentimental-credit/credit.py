8fccaeeac8ec155c2314ceb8331673feb3e35d0b
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-12 00:06:40 +0000

    Wed, Oct 11, 2023, 6:06 PM -06:00

--- pset6/pset6/sentimental-credit/credit.py
+++ pset6/pset6/sentimental-credit/credit.py
@@ -3,14 +3,27 @@ from cs50 import get_int
 
 def checksum(credit):
     multiply = False
-    check
+    check = 0
     sum = 0
     length = 0
 
     while(credit > 0):
-        if (multiply == True):
-            check = credit % 10;
-        elif:
+        if (multiply):
+            check = credit % 10
+            if (check * 2 >= 10):
+                sum += 1
+                sum += check * 2 - 10
+            else:
+                sum += check * 2
+        # The // operator in Python performs integer division, which is equivalent to the / operator in C when both operands are integers.
+            credit //= 10
+            multiply = False
+        else:
+            check = credit % 10
+            sum += check
+            credit //= 10
+            multiply = True
+        length += 1
 
 
 credit_number = get_int("Input the credit card number: ")
