95cc3d3fc0accabdd77a418e0965f1e4e795b5c7
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-12 21:10:42 +0000

    Thu, Oct 12, 2023, 3:10 PM -06:00

--- pset6/pset6/sentimental-readability/readability.py
+++ pset6/pset6/sentimental-readability/readability.py
@@ -1,3 +1,12 @@
 # TODO
 
-text[] = input("Text: ")
+# Convert the input string into a list of words,
+# with split() -> This method splits a string into a list where each word is a separate element.
+text = input("Text: ")
+
+# Now, words is a list of words.
+words = text.split()
+
+# Get the number of words with len(words).
+for word in words:
+    print(len(word))
