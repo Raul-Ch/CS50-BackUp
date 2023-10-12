5abaea3f6631b39cf08584c3d6c6a12d80659067
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-12 21:36:09 +0000

    Thu, Oct 12, 2023, 3:36 PM -06:00

--- pset6/pset6/sentimental-readability/readability.py
+++ pset6/pset6/sentimental-readability/readability.py
@@ -8,9 +8,16 @@ text = input("Text: ")
 word = text.split()
 
 # Number of words
-words = len(words)
+words = len(word)
 
 # Get the number of letters with len(words).
 letters = 0
+sentences = 0
+
 for letter in word:
-    letters =+ 1
+        letters =+ 1
+        print(letter)
+        if letter == '.':
+            sentences =+ 1
+
+print(words, letters, sentences)
