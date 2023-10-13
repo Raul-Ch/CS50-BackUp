1ddba1a51b38dbc9719c2b1c02f0cf3349b9bbc1
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-13 17:53:05 +0000

    Fri, Oct 13, 2023, 11:53 AM -06:00

--- pset7/lecture7/favorites.py
+++ pset7/lecture7/favorites.py
@@ -14,3 +14,6 @@ with open("favorites.csv", "r") as file:
     # Iterate over CSV file, printing each favorite
     for row in reader:
         print(row[1])
+
+# Notice that the csv library is imported. Further, we created a reader that will hold the result of csv.reader(file).
+# The csv.reader function reads each row from the file, and in our code we store the results in reader. print(row[1]), therefore, will print the language from the favorites.csv file.
