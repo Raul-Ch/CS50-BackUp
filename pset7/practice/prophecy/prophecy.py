6a21deec46c8b88048fce717cb0c5d6d043f7519
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-24 16:52:14 +0000

    Tue, Oct 24, 2023, 10:52 AM -06:00

--- pset7/practice/prophecy/prophecy.py
+++ pset7/practice/prophecy/prophecy.py
@@ -1,28 +1,31 @@
 from cs50 import SQL
 import csv
 
-#LISTS
+# LISTS
 students = []
 houses = []
 assignments = []
 
+
 def students_Table(student):
     students.append({"Student": student})
 
+
 def houses_Table(house, head):
-    if not any(h['House'] == house for h in houses):
+    if not any(h["House"] == house for h in houses):
         houses.append({"House": house, "Head": head})
+
 
 def assignments_Table(student, house):
     assignments.append({"Student": student, "House": house})
 
-#DB
+
+# DB
 # Open database
 db = SQL("sqlite:///roster.db")
 
 # Open CSV file
 with open("students.csv", "r") as file:
-
     # Create reader
     reader = csv.DictReader(file)
 
@@ -40,13 +43,29 @@ with open("students.csv", "r") as file:
         db.execute("INSERT INTO students (student_name) VALUES (?)", student["Student"])
 
     for house in houses:
-        db.execute("INSERT INTO houses (house, head) VALUES (?,?)", house["House"], house["Head"], )
+        db.execute(
+            "INSERT INTO houses (house, head) VALUES (?,?)",
+            house["House"],
+            house["Head"],
+        )
 
     for assignment in assignments:
-        student_id = db.execute("SELECT id FROM students WHERE student_name = ?", assignment["Student"])[0]['id']
-        house_id = db.execute("SELECT id FROM houses WHERE house = ?", assignment["House"])[0]['id']
-        
-        db.execute("INSERT INTO assignments (id_students, id_houses) VALUES (?, ?)", student_id, house_id)
+        student_id = db.execute(
+            "SELECT id FROM students WHERE student_name = ?", assignment["Student"]
+        )[0]["id"]
+        house_id = db.execute(
+            "SELECT id FROM houses WHERE house = ?", assignment["House"]
+        )[0]["id"]
+
+        db.execute(
+            "INSERT INTO assignments (id_students, id_houses) VALUES (?, ?)",
+            student_id,
+            house_id,
+        )
+
+# For each assignment, you're executing a SELECT query to get the ID of the student and the house from their respective tables.
+## Then, you're inserting these IDs into the assignments table. This process is repeated for each assignment in the assignments list.
+### So, if your assignments list has 10 elements, the loop will run 10 times, and you'll end up with 10 rows in your assignments table, each corresponding to an assignment.
 
 
 """     print(houses)
