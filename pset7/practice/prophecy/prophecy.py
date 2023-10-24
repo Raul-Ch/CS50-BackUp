d8ce06132b80fde3b0ead37c1fc668a3f0ce5359
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-24 04:29:16 +0000

    Mon, Oct 23, 2023, 10:29 PM -06:00

---
+++ pset7/practice/prophecy/prophecy.py
@@ -0,0 +1,11 @@
+import csv
+
+# Open CSV file
+with open("students.csv", "r") as file:
+
+    # Create reader
+    reader = csv.reader(file)
+
+    # Iterate over CSV file, printing each favorite
+    for row in reader:
+        print(row)
