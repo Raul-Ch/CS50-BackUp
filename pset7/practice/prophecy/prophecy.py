531cfaa49000aae813946ac481b979d512da3958
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-24 05:00:10 +0000

    Mon, Oct 23, 2023, 11:00 PM -06:00

--- pset7/practice/prophecy/prophecy.py
+++ pset7/practice/prophecy/prophecy.py
@@ -13,10 +13,12 @@ with open("students.csv", "r") as file:
 
     # Iterate over CSV file,
     for row in reader:
+        print("___________________")
         students = row["student_name"]
+        print(students)
         houses = row["house"]
+        print(houses)
         heads = row["head"]
+        print(heads)
+        print("___________________")
 
-
-for student in students:
-    print(students[student])
