44c9837fe488a769ef36296a58eea85a73106ae8
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-24 04:38:58 +0000

    Mon, Oct 23, 2023, 10:38 PM -06:00

--- pset7/practice/prophecy/schema.sql
+++ pset7/practice/prophecy/schema.sql
@@ -1,21 +1,21 @@
 CREATE TABLE students (
-    id INTEGER UNIQUE,
-    student_name TEXT,
+    id INTEGER UNIQUE NOT NULL,
+    student_name TEXT NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE houses (
-    id INTEGER UNIQUE,
-    house TEXT,
-    head TEXT,
+    id INTEGER UNIQUE NOT NULL,
+    house TEXT NOT NULL,
+    head TEXT NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE assignments (
-    id INTEGER UNIQUE,
-    id_students INTEGER,
-    id_houses INTEGER,
+    id INTEGER UNIQUE NOT NULL,
+    id_students INTEGER NOT NULL,
+    id_houses INTEGER NOT NULL,
     PRIMARY KEY(id)
-    FOREIGN KEY(id_students as students.id)
-    FOREIGN KEY(id_houses as houses.id)
+    FOREIGN KEY(id_students) REFERENCES students(id),
+    FOREIGN KEY(id_houses) REFERENCES houses(id),
 );
