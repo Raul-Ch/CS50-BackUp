2c05bbeaf98b7957b059fe3234df8ce04190da46
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-25 20:21:21 +0000

    Wed, Oct 25, 2023, 2:21 PM -06:00

--- pset7/pset7/fiftyville/log.sql
+++ pset7/pset7/fiftyville/log.sql
@@ -144,7 +144,9 @@ WITH Q1 AS (
   SELECT passengers.passport_number, people.name FROM passengers
   INNER JOIN flights ON passengers.flight_id = flights.id
   INNER JOIN people ON passengers.passport_number = people.passport_number
-  WHERE flight_id IN ...
+  WHERE flight_id IN
+  (SELECT id FROM flights WHERE origin_airport_id IN
+  (SELECT id FROM airports WHERE city = "Fiftyville") AND day = 29 AND month = 7 AND year = 2021 ORDER BY month, day, hour, minute LIMIT 1)
 ),
 Q2 AS (
   SELECT * FROM
@@ -155,13 +157,4 @@ Q2 AS (
 )
 SELECT * FROM Q1 INNER JOIN Q2 ON Q1.name = Q2.name;
 
-SELECT passengers.passport_number, people.name FROM passengers
-INNER JOIN flights ON passengers.flight_id = flights.id
-INNER JOIN people ON passengers.passport_number = people.passport_number
-WHERE flight_id IN
-(SELECT id FROM flights WHERE origin_airport_id IN
-(SELECT id FROM airports WHERE city = "Fiftyville") AND day = 29 AND month = 7 AND year = 2021 ORDER BY month, day, hour, minute LIMIT 1) AS Q1 INNER JOIN (SELECT * FROM
-(SELECT * FROM phone_calls WHERE day = 28 AND month = 7 AND year = 2021 and duration < 60) AS calls
-INNER JOIN people AS caller ON calls.caller = caller.phone_number
-INNER JOIN people AS receiver ON calls.receiver = receiver.phone_number
-WHERE (caller.name = "Diana" OR caller.name = "Taylor")) AS Q2 ON Q1.name == Q2.name;
+-- RESULT 
