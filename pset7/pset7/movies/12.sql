d7d9c134c9ee0f24f0f33933f026d5167f67f1be
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-25 02:10:04 +0000

    Tue, Oct 24, 2023, 8:10 PM -06:00

--- pset7/pset7/movies/12.sql
+++ pset7/pset7/movies/12.sql
@@ -2,4 +2,4 @@
 -- Your query should output a table with a single column for the title of each movie.
 -- You may assume that there is only one person in the database with the name Bradley Cooper.
 -- You may assume that there is only one person in the database with the name Jennifer Lawrence.
-SELECT title FROM movies INNER JOIN stars ON movies.id = stars.movie_id INNER JOIN ratings ON movies.id = ratings.movie_id INNER JOIN people ON stars.person_id = people.id WHERE people.name = "Chadwick Boseman" ORDER BY rating DESC LIMIT 5;
+SELECT title FROM movies INNER JOIN stars ON movies.id = stars.movie_id INNER JOIN people ON stars.person_id = people.id WHERE people.name = "Bradley Cooper" AND people.name = "Jennifer Lawrence";
