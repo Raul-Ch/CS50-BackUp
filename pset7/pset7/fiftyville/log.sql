7540bf408618fe34baa1f235f84a2dbe58712072
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-25 19:34:50 +0000

    Wed, Oct 25, 2023, 1:34 PM -06:00

--- pset7/pset7/fiftyville/log.sql
+++ pset7/pset7/fiftyville/log.sql
@@ -87,3 +87,5 @@ ON Q1.id = Q2.person_id;
 -- 13.- Check the phone Calls on the same day of the robbery
 SELECT * FROM phone_calls WHERE day = 28 AND month = 7 AND year = 2021 and duration < 60;
 
+-- 14.- Compare last querie with suspects
+SELECT * FROM (SELECT * FROM phone_calls WHERE day = 28 AND month = 7 AND year = 2021 and duration < 60) INNER JOIN (SELECT * FROM people) ON phone_calls.id = people.id;
