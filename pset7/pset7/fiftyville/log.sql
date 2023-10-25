b15e0d5a291e8c36a6569ffcffd947ba945fe167
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-25 19:54:49 +0000

    Wed, Oct 25, 2023, 1:54 PM -06:00

--- pset7/pset7/fiftyville/log.sql
+++ pset7/pset7/fiftyville/log.sql
@@ -106,7 +106,9 @@ SELECT * FROM
 INNER JOIN people AS caller ON calls.caller = caller.phone_number
 INNER JOIN people AS receiver ON calls.receiver = receiver.phone_number
 WHERE (caller.name = "Diana" OR caller.name = "Taylor");
--- NOTES: THIEF: Taylor , ACCOMPLICE: James
+-- NOTES SUSPECT: THIEF: Taylor , ACCOMPLICE: James
 -- REAL LIKE: THIEF: Diana , ACCOMPLICE: Philip
+
+
 
 
