41186b8d0ae20bd731ded2c6ab6fb0779e01fab6
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-25 20:41:52 +0000

    Wed, Oct 25, 2023, 2:41 PM -06:00

--- pset7/pset7/fiftyville/log.sql
+++ pset7/pset7/fiftyville/log.sql
@@ -35,13 +35,13 @@ SELECT * FROM bakery_security_logs LIMIT 5;
 SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND hour = 10 AND minute > 20 AND activity = "exit";
     -- Notes: There are 2 plates within the 10 minutes mark at 10:23 that exit and one at 10:35 being;
         -- 322W7JE, 0NTHK55 and 1106N58
-        -- 
+        -- L93JTIZ
 
 -- CAR FOOTAGE
 -- 7.- Check the people table with the plates given
 SELECT * FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND hour = 10 AND minute > 15);
 -- or
-SELECT * FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE license_plate = "322W7JE" OR license_plate = "0NTHK55" OR license_plate = "1106N58");
+SELECT * FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE license_plate = "322W7JE" OR license_plate = "0NTHK55" OR license_plate = "1106N58" OR license_plate = "1106N58");
 /*
 |   id   |  name  |  phone_number  | passport_number | license_plate |
 +--------+--------+----------------+-----------------+---------------+
