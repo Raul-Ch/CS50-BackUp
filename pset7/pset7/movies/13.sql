eba92d50ec8d6d87258be613b85a0474f88628ed
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-25 02:33:03 +0000

    Tue, Oct 24, 2023, 8:33 PM -06:00

--- pset7/pset7/movies/13.sql
+++ pset7/pset7/movies/13.sql
@@ -5,5 +5,5 @@
 SELECT name FROM people WHERE id IN
     (SELECT person_id FROM stars WHERE movie_id IN
     (SELECT movie_id FROM stars INNER JOIN people ON stars.person_id = people.id
-    WHERE people.name = "Kevin Bacon" AND people.birth = 1958)
+    WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND name != "Kevin Bacon"
 );
