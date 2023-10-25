fec68d17cd65b51f44167e0776b04dfc13ddcc67
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-25 01:13:01 +0000

    Tue, Oct 24, 2023, 7:13 PM -06:00

--- pset7/pset7/movies/6.sql
+++ pset7/pset7/movies/6.sql
@@ -1,2 +1,3 @@
 -- In 6.sql, write a SQL query to determine the average rating of all movies released in 2012.
 -- Your query should output a table with a single column and a single row (not counting the header) containing the average rating.
+SELECT AVG(ratings.rating) FROM ratings WHERE ratings.movie_id ;
