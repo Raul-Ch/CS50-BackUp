68e649e5d651eca3e1424f67adbadecc073cf45f
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-25 20:15:22 +0000

    Wed, Oct 25, 2023, 2:15 PM -06:00

--- pset7/pset7/fiftyville/log.sql
+++ pset7/pset7/fiftyville/log.sql
@@ -132,6 +132,8 @@ SELECT passport_number FROM passengers WHERE flight_id IN
 (SELECT id FROM airports WHERE city = "Fiftyville") AND day = 29 AND month = 7 AND year = 2021 ORDER BY month, day, hour, minute LIMIT 1);
 
 -- 19.- Passports and names of assengers on fligth
-SELECT passport_number FROM passengers WHERE flight_id IN
-(SELECT destination_airport_id FROM flights WHERE origin_airport_id IN
-(SELECT id FROM airports WHERE city = "Fiftyville") AND day = 29 AND month = 7 AND year = 2021 ORDER BY month, day, hour, minute LIMIT 1) INNER JOIN people ON passengers.passport_number = people.passport_number;
+SELECT *, people.name FROM passengers
+INNER JOIN flights ON passengers.flight_id = flights.id
+WHERE flight_id IN
+(SELECT id FROM flights WHERE origin_airport_id IN
+(SELECT id FROM airports WHERE city = "Fiftyville") AND day = 29 AND month = 7 AND year = 2021 ORDER BY month, day, hour, minute LIMIT 1);
