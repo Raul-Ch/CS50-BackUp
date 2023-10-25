3891c3dde7b7a53e9ec1b3bc81a88d77032de829
Raul Chimal <raulchimalhdz@hotmail.com>
2023-10-25 02:08:02 +0000

    Tue, Oct 24, 2023, 8:08 PM -06:00

---
+++ pset7/pset7/movies/12.sql
@@ -0,0 +1,5 @@
+-- In 12.sql, write a SQL query to list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
+-- Your query should output a table with a single column for the title of each movie.
+-- You may assume that there is only one person in the database with the name Bradley Cooper.
+-- You may assume that there is only one person in the database with the name Jennifer Lawrence.
+SELECT title FROM movies INNER JOIN stars ON movies.id = stars.movie_id INNER JOIN ratings ON movies.id = ratings.movie_id INNER JOIN people ON stars.person_id = people.id WHERE people.name = "Chadwick Boseman" ORDER BY rating DESC LIMIT 5;
