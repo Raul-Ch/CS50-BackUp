2868f4033ce276078fc44f427dbae7fe291975ee
Raul Chimal <raulchimalhdz@hotmail.com>
2023-11-24 20:03:30 +0000

    Fri, Nov 24, 2023, 2:03 PM -06:00

---
+++ pset8/pset8/homepage/scripts/headerscript.js
@@ -0,0 +1,13 @@
+// Use a function to load the navigation bar into the specified container
+function loadNavbar() {
+    // Fetch the container element
+    var container = document.getElementById('navbar-container');
+
+    fetch('../navbar.html')  // Adjust the path to go up one level to the root directory
+      .then(response => response.text())
+      .then(data => {
+        container.innerHTML = data;
+      });
+  }
+
+  document.addEventListener('DOMContentLoaded', loadNavbar);
--- pset8/pset8/homepage/scripts/navbarScript.js
+++ pset8/pset8/homepage/scripts/navbarScript.js
@@ -1,9 +1,9 @@
 // Use a function to load the navigation bar into the specified container
 function loadNavbar() {
   // Fetch the container element
-  var container = document.getElementById('navbar-container');
+  var container = document.getElementById('header');
 
-  fetch('../navbar.html')  // Adjust the path to go up one level to the root directory
+  fetch('../header.html')  // Adjust the path to go up one level to the root directory
     .then(response => response.text())
     .then(data => {
       container.innerHTML = data;
