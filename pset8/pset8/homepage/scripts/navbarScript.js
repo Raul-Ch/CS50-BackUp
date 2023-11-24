37157b7a96a65499124c384dd777602bdd21f42d
Raul Chimal <raulchimalhdz@hotmail.com>
2023-11-24 19:28:37 +0000

    Fri, Nov 24, 2023, 1:28 PM -06:00

--- pset8/pset8/homepage/scripts/navbarScript.js
+++ pset8/pset8/homepage/scripts/navbarScript.js
@@ -1,13 +1,13 @@
 // Use a function to load the navigation bar into the specified container
 function loadNavbar() {
-    // Fetch the container element
-    var container = document.getElementById('navbar-container');
+  // Fetch the container element
+  var container = document.getElementById('navbar-container');
 
-    fetch('homepage/navbar.html')
-      .then(response => response.text())
-      .then(data => {
-        container.innerHTML = data;
-      });
-  }
+  fetch('../navbar.html')  // Adjust the path to go up one level to the root directory
+    .then(response => response.text())
+    .then(data => {
+      container.innerHTML = data;
+    });
+}
 
 loadNavbar();
