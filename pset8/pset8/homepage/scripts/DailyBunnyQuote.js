1654ad39ce28796432b8de643055a72955f6ecfa
Raul Chimal <raulchimalhdz@hotmail.com>
2023-11-26 00:12:38 +0000

    Sat, Nov 25, 2023, 6:12 PM -06:00

--- pset8/pset8/homepage/scripts/DailyBunnyQuote.js
+++ pset8/pset8/homepage/scripts/DailyBunnyQuote.js
@@ -9,8 +9,10 @@
             lines.shift();
 
             // Parse each line into a quote and an author
-            const [quote, author] = line.split(',');
-            return { quote: quote.trim().replace(/"/g, ''), author: author.trim().replace(/"/g, '') };
+            const quotes = lines.map(line => {
+                const [quote, author] = line.split(',');
+                return { quote: quote.trim().replace(/"/g, ''), author: author.trim().replace(/"/g, '') };
+            });
 
             // Function to display a random quote
             function getRandomQuote() {
