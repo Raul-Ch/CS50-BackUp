2d4d7e5c43c4a5df67157755ee6c6e32ba328320
Raul Chimal <raulchimalhdz@hotmail.com>
2023-11-26 00:01:27 +0000

    Sat, Nov 25, 2023, 6:01 PM -06:00

---
+++ pset8/pset8/homepage/scripts/DailyBunnyQuote.js
@@ -0,0 +1,34 @@
+// Fetch the CSV file using the Fetch API
+    fetch('yourfile.csv')
+        .then(response => response.text())
+        .then(data => {
+            // Split the CSV string into an array of lines
+            const lines = data.split('\n');
+
+            // Remove the header line (if present)
+            lines.shift();
+
+            // Parse each line into a quote and an author
+            const quotes = lines.map(line => {
+                const [quote, author] = line.split(',');
+                return { quote: quote.trim(), author: author.trim() };
+            });
+
+            // Function to display a random quote
+            function getRandomQuote() {
+                // Get a random index
+                const randomIndex = Math.floor(Math.random() * quotes.length);
+
+                // Get the random quote and author
+                const randomQuote = quotes[randomIndex];
+
+                // Display the quote and author on the webpage
+                document.getElementById('quote').textContent = randomQuote.quote;
+                document.getElementById('author').textContent = `- ${randomQuote.author}`;
+            }
+
+            // Initial display
+            getRandomQuote();
+        })
+        .catch(error => console.error('Error fetching the file:', error));
+
