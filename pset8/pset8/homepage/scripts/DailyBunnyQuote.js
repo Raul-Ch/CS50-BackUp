421517c3d362505132fb1a65a7c7b05bf4412e15
Raul Chimal <raulchimalhdz@hotmail.com>
2023-11-26 00:21:40 +0000

    Sat, Nov 25, 2023, 6:21 PM -06:00

--- pset8/pset8/homepage/scripts/DailyBunnyQuote.js
+++ pset8/pset8/homepage/scripts/DailyBunnyQuote.js
@@ -1,37 +1,40 @@
 // Fetch the CSV file using the Fetch API
-    fetch('../text/DailyBunnyQuote.csv')
-        .then(response => response.text())
-        .then(data => {
-            // Split the CSV string into an array of lines
-            const lines = data.split('\n');
+fetch('../text/DailyBunnyQuote.csv')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        // Split the CSV string into an array of lines
+        const lines = data.split('\n');
 
-            // Remove the header line (if present)
+        // Remove the header line (if present)
+        if (lines.length > 0) {
             lines.shift();
+        }
 
-            // Parse each line into a quote and an author
-            const quotes = lines.map(line => {
-                const [quote, author] = line.split(',');
-                if (!quote || !author) {
-                    console.log(`Issue with line: ${line}`);
-                }
-                return { quote: quote.trim().replace(/"/g, ''), author: author.trim().replace(/"/g, '') };
-            });
+        // Parse each line into a quote and an author
+        const quotes = lines.map(line => {
+            const [quote, author] = line.split(',');
+            return { quote: quote.trim().replace(/"/g, ''), author: author.trim().replace(/"/g, '') };
+        });
 
-            // Function to display a random quote
-            function getRandomQuote() {
-                // Get a random index
-                const randomIndex = Math.floor(Math.random() * quotes.length);
+        // Function to display a random quote
+        function getRandomQuote() {
+            // Get a random index
+            const randomIndex = Math.floor(Math.random() * quotes.length);
 
-                // Get the random quote and author
-                const randomQuote = quotes[randomIndex];
+            // Get the random quote and author
+            const randomQuote = quotes[randomIndex];
 
-                // Display the quote and author on the webpage
-                document.getElementById('quote').textContent = randomQuote.quote;
-                document.getElementById('author').textContent = `- ${randomQuote.author}`;
-            }
+            // Display the quote and author on the webpage
+            document.getElementById('quote').textContent = randomQuote.quote;
+            document.getElementById('author').textContent = `- ${randomQuote.author}`;
+        }
 
-            // Initial display
-            getRandomQuote();
-        })
-        .catch(error => console.error('Error fetching the file:', error));
-
+        // Initial display
+        getRandomQuote();
+    })
+    .catch(error => console.error('Error fetching the file:', error));
