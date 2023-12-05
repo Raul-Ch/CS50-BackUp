7831c4a807bbf1452477e2a841fac39fe2612e40
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-05 02:51:25 +0000

    Mon, Dec 4, 2023, 8:51 PM -06:00

--- pset9/lecture9/app.py
+++ pset9/lecture9/app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name=request.args.get("name", "world"))
+    return render_template("index.html")
 
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
 
 # Notice that this code defines app as the Flask application. Then, it defines the / route of app as returning the contents of index.html with the argument of name. By default, the request.args.get function will look for the name being provided by the user. If no name is provided, it will default to world.
