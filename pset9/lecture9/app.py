26cdf075e3620f9825ae0da4d4f15da5381365c5
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-05 02:59:15 +0000

    Mon, Dec 4, 2023, 8:59 PM -06:00

--- pset9/lecture9/app.py
+++ pset9/lecture9/app.py
@@ -1,4 +1,4 @@
-# Uses a single route
+# Greets user
 
 from flask import Flask, render_template, request
 
@@ -10,3 +10,6 @@ def index():
     if request.method == "POST":
         return render_template("greet.html", name=request.form.get("name", "world"))
     return render_template("index.html")
+# Notice that this code defines app as the Flask application. Then, it defines the / route of app as returning the contents of index.html with the argument of name. By default, the request.args.get function will look for the name being provided by the user. If no name is provided, it will default to world.
+
+#
