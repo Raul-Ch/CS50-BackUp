4bd547d11037a22c984a2fdd7ed76b9862fae0f1
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-11 23:26:09 +0000

    Mon, Dec 11, 2023, 5:26 PM -06:00

--- pset9/pset9/finance/static/scripts.js
+++ pset9/pset9/finance/static/scripts.js
@@ -9,14 +9,15 @@ function validatePassword() {
     // Check if the password meets the requirements
     if (passwordRegex.test(password)) {
         console.log("Password is valid");
-        errorLabel.className = 'good_password';
         document.getElementById('Consideration').textContent = "Consideration:";
         document.getElementById('Note').textContent = ": Good password!";
+        document.getElementById('Consideration').className = 'good_password';
+        document.getElementById('Note').className = 'good_password';
     } else {
         console.log("Password is invalid");
         document.getElementById('Consideration').textContent = "Consideration: ";
         document.getElementById('Note').textContent = "User passwords need to have some number of letters, numbers, and/or symbols.";
-        errorLabel.className = 'bad_password';
-        parentElement.insertBefore(errorLabel, document.getElementById('password'));
+        document.getElementById('Consideration').className = 'validate_password';
+        document.getElementById('Note').className = 'validate_password';
     }
 }
