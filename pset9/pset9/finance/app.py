ea1204433cf63d6e6c6a289416cde8d73e004e21
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-11 22:22:03 +0000

    Mon, Dec 11, 2023, 4:22 PM -06:00

--- pset9/pset9/finance/app.py
+++ pset9/pset9/finance/app.py
@@ -109,7 +109,7 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    if request.method == "POST":
+    if request.method == "GET":
         return register("/")
 
     else:
