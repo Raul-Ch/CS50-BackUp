579a315079cd191c9440933524a8afd4f574c128
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-11 23:28:15 +0000

    Mon, Dec 11, 2023, 5:28 PM -06:00

--- pset9/pset9/finance/static/scripts.js
+++ pset9/pset9/finance/static/scripts.js
@@ -2,7 +2,7 @@
 function validatePassword() {
     console.log("validatePassword function called");
     var password = document.getElementById('password').value;
-    var passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d|\W).+$/;
+    var passwordRegex = /^(?=(?:.*[a-zA-Z]){8})(?=(?:.*\d){2})(?=(?:.*\W){1}).{11,}$/;
     var parentElement = document.getElementById('password').parentNode;
     var errorLabel = document.getElementById('errorLabel');
 
@@ -16,7 +16,7 @@ function validatePassword() {
     } else {
         console.log("Password is invalid");
         document.getElementById('Consideration').textContent = "Consideration: ";
-        document.getElementById('Note').textContent = "User passwords need to have some number of letters, numbers, and/or symbols.";
+        document.getElementById('Note').textContent = "User passwords need to have at least 8 number of letters, 2 numbers, and 1 symbol.";
         document.getElementById('Consideration').className = 'validate_password';
         document.getElementById('Note').className = 'validate_password';
     }
