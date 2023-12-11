c36f972ec6b1139d22c04ae9eaa866a3f8b53798
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-11 22:55:54 +0000

    Mon, Dec 11, 2023, 4:55 PM -06:00

--- pset9/pset9/finance/scripts.js
+++ pset9/pset9/finance/scripts.js
@@ -1,18 +1,30 @@
-
-// Add an event listener to the password input field
-document.getElementById('password').addEventListener('input', validatePassword);
+// scripts.js
 
 // Function to validate the password
 function validatePassword() {
     var password = document.getElementById('password').value;
     var passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d|\W).+$/;
+    var parentElement = document.getElementById('password').parentNode;
+    var errorLabel = document.getElementById('errorLabel');
 
     // Check if the password meets the requirements
     if (passwordRegex.test(password)) {
-        document.getElementById('Consideration').textContent = "Consideration:";
-        document.getElementById('Note').textContent = "Good password!";
+        // Password is valid, remove the error label if it exists
+        if (errorLabel) {
+            parentElement.removeChild(errorLabel);
+        }
+        document.getElementById('Consideration').textContent = "Consideration: Good password!";
+        document.getElementById('Note').textContent = "";
     } else {
+        // Password is invalid, create and insert the error label if it doesn't exist
+        if (!errorLabel) {
+            errorLabel = document.createElement('label');
+            errorLabel.id = 'errorLabel';
+            errorLabel.textContent = "User passwords need to have some number of letters, numbers, and/or symbols.";
+            errorLabel.className = 'bad_password';
+            parentElement.insertBefore(errorLabel, document.getElementById('password'));
+        }
         document.getElementById('Consideration').textContent = "Consideration: ";
-        document.getElementById('Note').textContent = "User passwords need to have some number of letters, numbers, and/or symbols.";
+        document.getElementById('Note').textContent = "";
     }
 }
