66fc564a54fada64fd4f636b035f4e336e2b3109
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-11 23:21:52 +0000

    Mon, Dec 11, 2023, 5:21 PM -06:00

--- pset9/pset9/finance/static/scripts.js
+++ pset9/pset9/finance/static/scripts.js
@@ -9,22 +9,14 @@ function validatePassword() {
     // Check if the password meets the requirements
     if (passwordRegex.test(password)) {
         console.log("Password is valid");
-        // Password is valid, remove the error label if it exists
-        if (errorLabel) {
-            parentElement.removeChild(errorLabel);
-        }
+        errorLabel.className = 'good_password';
         document.getElementById('Consideration').textContent = "Consideration:";
         document.getElementById('Note').textContent = ": Good password!";
     } else {
         console.log("Password is invalid");
-        // Password is invalid, create and insert the error label if it doesn't exist
-        if (!errorLabel) {
-            errorLabel = document.createElement('label');
-            errorLabel.id = 'errorLabel';
-            document.getElementById('Consideration').textContent = "Consideration: ";
-            document.getElementById('Note').textContent = "User passwords need to have some number of letters, numbers, and/or symbols.";
-            errorLabel.className = 'bad_password';
-            parentElement.insertBefore(errorLabel, document.getElementById('password'));
-        }
+        document.getElementById('Consideration').textContent = "Consideration: ";
+        document.getElementById('Note').textContent = "User passwords need to have some number of letters, numbers, and/or symbols.";
+        errorLabel.className = 'bad_password';
+        parentElement.insertBefore(errorLabel, document.getElementById('password'));
     }
 }
