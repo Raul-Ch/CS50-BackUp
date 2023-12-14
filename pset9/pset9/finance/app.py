7a05148ab34c426c71a20d749293a59393732870
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-14 19:58:44 +0000

    Thu, Dec 14, 2023, 1:58 PM -06:00

--- pset9/pset9/finance/app.py
+++ pset9/pset9/finance/app.py
@@ -356,7 +356,7 @@ def sell():
                 db.execute("UPDATE users SET cash = ? WHERE id = ?", cash, user_id)
                 db.execute(
                     "INSERT INTO transactions (user_id, symbol, shares, price, timestamp, name, type) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?)",
-                    user_id, symbol, shares, dic_symbol["price"],dic_symbol["name"], 0
+                    user_id, symbol, shares, dic_symbol["price"],dic_symbol["name"], 1
                 )
                 transactions = db.execute("SELECT symbol, name, shares, price, timestamp, shares * price AS total FROM transactions WHERE user_id = ?", user_id)
                 total_rows = db.execute("SELECT SUM(shares * price) AS overall_total FROM transactions WHERE user_id = ?", user_id)
