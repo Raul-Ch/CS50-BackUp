59c275565bc855cf448beb8f42cf94506296ec61
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-14 04:59:44 +0000

    Wed, Dec 13, 2023, 10:59 PM -06:00

--- pset9/pset9/finance/app.py
+++ pset9/pset9/finance/app.py
@@ -42,7 +42,7 @@ def index():
     cash = "${:,.2f}".format(cash)
 
     transactions = db.execute("SELECT symbol, name, shares, price, timestamp, shares * price AS total FROM transactions WHERE user_id = ?", user_id)
-    total = db.execute("SELECT SUM(shares * price) AS overall_total FROM transactions WHERE user_id = ?", user_id)
+    total = db.execute(("SELECT cash FROM users") + ("SELECT SUM(shares * price) AS overall_total FROM transactions WHERE user_id = ?", user_id))
     total = total[0]['overall_total']
 
     return render_template("index.html", cash = cash, transactions = transactions, total = total)
