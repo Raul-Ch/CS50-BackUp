2634d5ae559844ea5f1129386a403c7226ad2889
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-17 01:19:21 +0000

    Sat, Dec 16, 2023, 7:19 PM -06:00

--- pset3/pset3/tideman/tideman.c
+++ pset3/pset3/tideman/tideman.c
@@ -224,11 +224,7 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
-    for (int i = 0; i < pair_count; i++){
-        locked[pairs[i].winner][pairs[i].loser] = false
-    }
-    return;
+
 }
 
 // Print the winner of the election
