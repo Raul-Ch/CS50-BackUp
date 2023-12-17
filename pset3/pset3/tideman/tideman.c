6b47ba45049060205093fd66e962843de6078179
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-17 01:42:30 +0000

    Sat, Dec 16, 2023, 7:42 PM -06:00

--- pset3/pset3/tideman/tideman.c
+++ pset3/pset3/tideman/tideman.c
@@ -36,7 +36,7 @@ void sort_pairs(void);
 void lock_pairs(void);
 void print_winner(void);
 
-void helper_lock_pairs(void);
+void helper_lock_pairs(int winner, int loser);
 
 // Debug
 void print_preferences(int ranks[]);
@@ -237,14 +237,15 @@ void lock_pairs(void) {
     }
 }
 
-void helper_lockpairs(void) {
+void helper_lockpairs(int winner, int loser) {
     // Keep track of which nodes (candidates) have been visited bya boolean array
     bool visited[candidates] = {false};
 
     // Start at a node (in this case, a candidate).
     // CS50 note: Remember, it should start at the loser and try to find a path back to the winner.
   for (int i = 0; i < pair_count; i++) {
-        start_candidate = pairs[i].loser;
+    int start_candidate = pairs[i].loser; // Represent a candidate's index
+
     //Mark the node as visited.
 
     //For each node connected to the current node, if it's not visited, recursively visit that node.
