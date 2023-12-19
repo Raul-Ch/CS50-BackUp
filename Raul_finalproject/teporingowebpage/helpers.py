558834aa579d8841c1d80e1c801e2e879ebac9d5
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-19 22:25:53 +0000

    Tue, Dec 19, 2023, 4:25 PM -06:00

--- Raul_finalproject/teporingowebpage/helpers.py
+++ Raul_finalproject/teporingowebpage/helpers.py
@@ -1,7 +1,11 @@
 import csv
 import datetime
+#This is a Python library that allows accurate and cross-platform timezone calculations. It's often used when you need to deal with time data that's timezone-aware.
 import pytz
+# This is a popular Python library for making HTTP requests. It abstracts the complexities of making requests behind a beautiful, simple API so that you can focus on interacting with services and consuming data in your application.
 import requests
-import subprocess
+# This is a Python module for fetching URLs (Uniform Resource Locators). It offers a very simple interface for doing complex tasks like opening, reading, and closing URLs.
 import urllib
-import uuid
+
+from flask import redirect, render_template,
+from functools import wraps
